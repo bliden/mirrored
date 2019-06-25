fb70f7fac82e34c0ffab6a582e8f1e919ae0cdaa
Benjamin Liden <benjamin.c.liden@gmail.com>
2019-06-24 20:31:52 -0400

    added load beer text button. improved some styling in controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export class Controls extends Component {
+  loadText = () => {
+    axios
+      .get("https://api.punkapi.com/v2/beers/random")
+      .then(response => {
+        const beer = response.data[0];
+        this.props.updateText(
+          `${beer.name}. ${beer.tagline} ${beer.description}`
+        );
+      })
+      .catch(err => console.error(err));
+  };
   handleChange = e => {
     return this.props.updateText(e.target.value);
   };
@@ -16,9 +28,22 @@ export class Controls extends Component {
             onChange={this.handleChange}
           />
         </label>
+        <button onClick={this.loadText} style={btnStyle}>
+          Load Random
+        </button>
       </div>
     );
   }
 }
+
+const btnStyle = {
+  color: "white",
+  padding: "5px 10px",
+  backgroundColor: "var(--purple)",
+  border: "1px solid var(--dark-blue)",
+  cursor: "pointer",
+
+  marginLeft: "0.5rem"
+};
 
 export default Controls;
